343d69ce16a0352db4adeaee59a74467b34ffd6c
Daniel Bastos <carlos.bastos@trf1.jus.br>
2023-11-17 13:32:53 -0300

    Kotlin JPA (finished) - Session 4:18

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,33 +1,35 @@
 import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.ir.backend.js.compile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 
 plugins {
-   // kotlin("jvm")
     kotlin("plugin.spring") version embeddedKotlinVersion
-    //kotlin("plugin.jpa") version embeddedKotlinVersion
     application
 }
 
 
 apply {
     plugin("org.springframework.boot")
-   // plugin("kotlin-jpa")
+}
+
+application {
+    mainClass.set("com.clicaqui.Config")
 }
 
 //buildscript {
 
     dependencies {
         val kotlinxHtmlVersion: String = properties["kotlinxHtmlVersion"] as String
-        val springBootVersion: String = properties["springBootVersion"] as String
-        val hibernateValidatorVersion: String = properties["hibernateValidatorVersion"] as String
+        val springBootVersion: String = project.properties["springBootVersion"] as String
+        val hibernateValidatorVersion: String = project.properties["hibernateValidatorVersion"] as String
         implementation(project(":backend:project"))
         //implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
         implementation("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
-        implementation("org.springframework.boot:spring-boot-devtools:$springBootVersion")
-        implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
+        compileOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
+        compileOnly("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
         implementation("org.hibernate:hibernate-validator:$hibernateValidatorVersion")
     }
 
@@ -45,9 +47,5 @@ apply {
         mainClass.set("com.clicaqui.Config")
 
     }
-
-application {
-    mainClass.set("com.clicaqui.Config")
-}
 
 //}
--- backend/project/build.gradle.kts
+++ backend/project/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
@@ -6,15 +5,13 @@ plugins {
     kotlin("plugin.spring") version embeddedKotlinVersion
     kotlin("plugin.jpa") version embeddedKotlinVersion
    // id("org.jetbrains.kotlin.plugin.jpa")
-  ///  application
 }
 
-//val kotlinVersion = properties["kotlinVersion"] as String
 
 dependencies {
-    val kotlinVersion = properties["kotlinVersion"] as String
-    val postgresVersion = "42.1.4"
-    implementation("org.springframework.boot:spring-boot-starter:2.6.0-M1")
+    val springBootVersion: String = properties["springBootVersion"] as String
+    val postgresVersion = properties["postgresVersion"] as String
+    implementation("org.springframework.boot:spring-boot-starter:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.1-SNAPSHOT")
     //implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.postgresql:postgresql:$postgresVersion")
